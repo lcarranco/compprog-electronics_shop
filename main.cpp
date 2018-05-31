9d9a589b340de4304472deaa4bddcad26ed8fd5b
lcarranco <lcarranco@gmail.com>
2018-05-31 17:58:30 -0500

    Getting budget, # of keyboards, and # of USB drives

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    int b = 0;
+    int n = 0;
+    int m = 0;
+    vector<int> keyboard;
+    vector<int> drives;
+
+    cin >> b;
+    cout << b << endl;
+    cin >> n;
+    cout << n << endl;
+    cin >> m;
+    cout << m << endl;
+    
+    
+
     return 0;
 }
