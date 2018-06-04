6514ddba3f93c264d69f45be0020502a2a1de20a
lcarranco <lcarranco@gmail.com>
2018-06-04 17:10:39 -0500

    Set sum

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@ int main() {
     int n = 0;
     int m = 0;
     int value = 0;
-    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+    int previous1 = 0;
+    int previous2 = 0;
     int max = 0;
     int min = 0;
     string line;
@@ -36,24 +39,11 @@ int main() {
         }
     }
 
-    if (keyboards.empty() == true && drives.empty() == true) {
+    if (keyboards.empty() == true || drives.empty() == true) {
         cout << "-1" << endl;
         return 0;
     }
 
-    set<int>::iterator kit = --keyboards.end();
-    max = *kit;
-    // cout << max << endl;
-    // cout << *kit << endl;
-    set<int>::iterator uit = drives.begin();
-    min = *uit;
-    // cout << min << endl;
-    // while (sum <= b) {
-
-    // }
-
-    
-    
     // cout << "Keyboard prices: ";
     // for (set<int>::iterator it = keyboards.begin(); it != keyboards.end(); ++it) {
     //     cout << *it << ' ';
@@ -64,6 +54,51 @@ int main() {
     // for (set<int>::iterator it = drives.begin(); it != drives.end(); ++it) {
     //     cout << *it << ' ';
     // }
+    // cout << "\n\n";
+
+    // Get sum with max keyboard and min drives
+    // cout << "Max keyboard and min drives" << endl;
+    set<int>::iterator kit = --keyboards.end();
+    max = *kit;
+    // cout << "Max: " << max << endl;
+    set<int>::iterator dit = drives.begin();
+    min = *dit;
+    // cout << "Min: " << min << endl;
+    sum1 = max + min;
+    // cout << "Sum1: " << sum1 << endl;
+    while (sum1 <= b) {
+        previous1 = sum1;
+        // cout << "Previous1: " << previous1 << endl;
+        ++dit;
+        sum1 = max + *dit;
+        // cout << "Sum1: " << sum1 << endl;
+    }
+    // cout << "\n\n";
+    
+    // Get sum with min keyboard and max drives
+    // cout << "Max keyboard and min drives" << endl;
+    dit = --drives.end();
+    max = *dit;
+    // cout << "Max: " << max << endl;
+    kit = keyboards.begin();
+    min = *kit;
+    // cout << "Min: " << min << endl;
+    sum2 = max + min;
+    // cout << "Sum2: " << sum2 << endl;
+    while (sum2 <= b) {
+        previous2 = sum2;
+        // cout << "Previous2: " << previous2 << endl;
+        ++kit;
+        sum2 = max + *kit;
+        // cout << "Sum2: " << sum2 << endl;
+    }
+
+    if (previous1 < previous2) {
+        // cout << "Max sum: " << previous2 << endl;
+        cout << previous2 << endl;
+        return 0;
+    }
+    cout << "-1" << endl;
 
     return 0;
 }
